0ed269c32b34836de9009aaecd5cc7a3b31189b9
kamil-dev <kamil.przylepa@gmail.com>
2020-12-28 16:58:16 +0100

    you can download

--- die_visual.py
+++ die_visual.py
@@ -1,10 +1,24 @@
+from plotly.graph_objs import Bar, Layout
+from plotly import offline
+
 from die import Die
+
 """utworzenie kosci typu d6"""
 die = Die()
 """Wykonanie pewnej ilosci rzutow i umieszczenie wynikow na liscie"""
 results = []
-for roll_results in range(100):
+for roll_results in range(1000):
     result = die.roll()
     results.append(result)
 
-print(results)
+frequiences = []
+for value in range(1, die.num_sides+1):
+    frequency = results.count(value)
+    frequiences.append(frequency)
+
+x_values = list(range(1, die.num_sides+1))
+data = [Bar(x=x_values, y=frequiences)]
+x_axis_config = {'title': 'Wynik'}
+y_axis_config = {'title': 'Częstotliwosc wystepowania wartosci'}
+my_layout = Layout(title='Wynik rzucania pojedyncza koscia D6 tysiac razy', xaxis=x_axis_config, yaxis=y_axis_config)
+offline.plot({'data': data, 'layout': my_layout}, filename='d6.html')
